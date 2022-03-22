7c8f7c814a8ed5733b0b9eb2e2eebd72d47094a9
Benedetto Marco Serinelli <silsniper@gmail.com>
2022-03-22 17:09:56 +0100

    Add log example

---
+++ esercitrazionejava/src/main/java/it/devlec/log/EsempioLog.java
@@ -0,0 +1,15 @@
+package it.devlec.log;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EsempioLog {
+    private static final Logger logger =  LogManager.getLogger(EsempioLog.class);
+
+    public EsempioLog(){
+
+    }
+    public void stampaAltriLog(){
+        logger.info("Ciao da stampa altri log...");
+    }
+}
