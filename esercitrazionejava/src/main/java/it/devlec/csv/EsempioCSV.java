fc164d5faba3cd7fb0f6184cb336f7613476c656
Benedetto Marco Serinelli <silsniper@gmail.com>
2022-03-22 17:08:35 +0100

    Add csv

---
+++ esercitrazionejava/src/main/java/it/devlec/csv/EsempioCSV.java
@@ -0,0 +1,67 @@
+package it.devlec.csv;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EsempioCSV {
+    private static final Logger logger = LogManager.getLogger(EsempioCSV.class);
+    private String[] INTESTAZIONE = {"autore", "titolo"};
+    private Map<String, String> MIEI_AUTORI = new HashMap<>() {
+        {
+            put("Dante Aligheri", "La divina commedia");
+            put("Giacomo Leopardi", "L'infinito");
+            put("Eugenio Montale", "Ossi di seppia");
+        }
+    };
+    public EsempioCSV() {
+    }
+    public void leggiCSV() {
+        Reader in = null;
+        try {
+            in = new FileReader("esercitrazionejava/src/main/resources/esempio.csv");
+            // Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT.builder().setSkipHeaderRecord(true)
+                    .setHeader(INTESTAZIONE).build().parse(in);
+            for (CSVRecord record : records) {
+                String autore = record.get(INTESTAZIONE[0]);
+                logger.info("Autore: " + autore);
+                String titolo = record.get(1);
+                logger.warn("Titolo: " + titolo);
+            }
+        } catch (IOException e) {
+            logger.error("Si è verificato un errore", e);
+        }
+    }
+    public void scriviCSV(){
+        try {
+            String mieiAutoriCSVPath = "esercitrazionejava/src/main/resources/mieiAutori.csv";
+            File mioCSV = new File(mieiAutoriCSVPath);
+            if(mioCSV.exists()){
+                logger.debug("Elimino il vecchio CSV");
+                mioCSV.delete();
+            }
+            boolean fileCreato = mioCSV.createNewFile();
+            if(fileCreato){
+                logger.debug("File CSV creato correttamente");
+            }
+            FileWriter out = new FileWriter(mieiAutoriCSVPath);
+            try (CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT)) {
+                MIEI_AUTORI.forEach((author, title) -> {
+                    try {
+                        printer.printRecord(author, title);
+                    } catch (IOException e) {
+                        logger.error("Si è verificato un errore nel scrivere i miei autori", e);                    }
+                });
+            }
+        } catch (IOException e) {
+            logger.error("Si è verificato un errore", e);
+        }
+    }
+}
